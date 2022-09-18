c21b1e7620ffd9f2379aa2ea7bf44e56034878e7
Jimin <kaley0421@naver.com>
2022-09-19 00:11:39 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/tgCalendar/tgCalendar/TgCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.tgCalendar.tgCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgCalendar/tgCalendar/TgCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgCalendar.tgCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
