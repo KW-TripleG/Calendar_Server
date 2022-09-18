a7c5d276476ce6284ff21815757090c8730525e5
Jimin <kaley0421@naver.com>
2022-09-19 00:18:46 +0900

    프로젝트 초기 설정

--- src/main/java/com/tgCalendar/tgCalendar/TgCalendarApplication.java
+++ src/main/java/com/tgCalendar/tgCalendar/TgCalendarApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TgCalendarApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TgCalendarApplication.class, args);
 	}
