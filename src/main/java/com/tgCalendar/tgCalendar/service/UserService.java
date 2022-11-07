12f349f4deb0c644ccfbe3b8bb1e58aaeb0e3afb
Jimin <kaley0421@naver.com>
2022-11-07 22:47:36 +0900

    로그인, 회원가입 코드 수정 (리팩토링 필요)

--- src/main/java/com/tgCalendar/tgCalendar/service/UserService.java
+++ src/main/java/com/tgCalendar/tgCalendar/service/UserService.java
@@ -4,6 +4,7 @@ import com.tgCalendar.tgCalendar.dto.UserDto;
 import com.tgCalendar.tgCalendar.entity.User;
 import com.tgCalendar.tgCalendar.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,15 +13,23 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-//    public UserDto findById(String id) {
-//        Optional<User> user = userRepository.findById(id);
-//
-//        if(user.isPresent()){
-//
-//
-//        }
-//
-//    }
+    public String saveUser(UserDto user) {
+        String userId = userRepository.save(User.builder()
+                        .id(user.getId())
+                        .password(passwordEncoder.encode(user.getPassword()))
+                        .email(user.getEmail())
+                        .nickName(user.getNickName())
+                        .build())
+                .getId();
 
+        return userId;
+    }
+
+    public User findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+
+        return user.orElse(null);
+    }
 }
--- src/main/java/com/tgCalendar/tgCalendar/v1/UserController.java
+++ src/main/java/com/tgCalendar/tgCalendar/v1/UserController.java
@@ -1,8 +1,10 @@
 package com.tgCalendar.tgCalendar.v1;
 
+import com.tgCalendar.tgCalendar.dto.UserDto;
 import com.tgCalendar.tgCalendar.entity.User;
 import com.tgCalendar.tgCalendar.security.jwt.JwtTokenProvider;
 import com.tgCalendar.tgCalendar.repository.UserRepository;
+import com.tgCalendar.tgCalendar.service.UserService;
 import com.tgCalendar.tgCalendar.util.Response;
 import com.tgCalendar.tgCalendar.util.StatusEnum;
 import lombok.RequiredArgsConstructor;
@@ -21,40 +23,58 @@ public class UserController {
 
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
-    private final UserRepository userRepository;
+
+    private final UserService userService;
 
     // 회원가입
     @PostMapping("/join")
-    public ResponseEntity<Response> join(@RequestBody Map<String, String> user) {
+    public ResponseEntity<Response> join(@RequestBody UserDto user) {
+        if (userService.findById(user.getId()) != null) {
+            Response body = Response.builder()
+                    .status(StatusEnum.BAD_REQUEST)
+                    .message("이미 존재하는 유저입니다.")
+                    .build();
+            return new ResponseEntity<>(body, Response.getDefaultHeader(), HttpStatus.BAD_REQUEST);
+        }
 
-        String userId = userRepository.save(User.builder()
-                                                .id(user.get("id"))
-                                                .password(passwordEncoder.encode(user.get("password")))
-                                                .email(user.get("email"))
-                                                .nickName(user.get("nickname"))
-                                                .build())
-                                        .getId();
+        String savedUserId = userService.saveUser(user);
 
         Response body = Response.builder()
                 .status(StatusEnum.OK)
-                .data(userId)
-                .message("성공")
+                .data(savedUserId)
+                .message("회원가입 성공")
                 .build();
-
         return new ResponseEntity<>(body, Response.getDefaultHeader(), HttpStatus.OK);
     }
 
     // 로그인
     @PostMapping("/login")
-    public String login(@RequestBody Map<String, String> user) {
-        User member = userRepository.findById(user.get("id"))                                   // DB 에 애초에 없는 id 면 가입되지 않은 것
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 유저입니다."));
+    public ResponseEntity<Response> login(@RequestBody Map<String, String> user) {
+        User member = userService.findById(user.get("id"));
 
-        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {             // 비밀번호 맞는지는 passwordEncoder 에서 확인
-            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        if (member == null) {                                                                     // DB 에 애초에 없는 id 면 가입되지 않은 것
+            Response body = Response.builder()
+                    .status(StatusEnum.BAD_REQUEST)
+                    .message("가입되지 않은 유저입니다.")
+                    .build();
+            return new ResponseEntity<>(body, Response.getDefaultHeader(), HttpStatus.BAD_REQUEST);
         }
 
-        return jwtTokenProvider.createToken(member.getUsername());                              // 존재하는 id 이고, 패스워드도 올바르다면 -> createToken 함수로 유저 id 정보를 담은 JWT 토큰 반환
-                                                                                                // 이후 클라이언트는 이 JWT 토큰을 'X-AUTH-TOKEN' 필드에 넣어서 요청
+        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {                 // 비밀번호 맞는지는 passwordEncoder 에서 확인
+            Response body = Response.builder()
+                    .status(StatusEnum.BAD_REQUEST)
+                    .message("잘못된 비밀번호입니다.")
+                    .build();
+            return new ResponseEntity<>(body, Response.getDefaultHeader(), HttpStatus.BAD_REQUEST);
+        }
+
+        Response body = Response.builder()
+                .status(StatusEnum.OK)
+                .data(jwtTokenProvider.createToken(member.getUsername()))
+                .message("로그인 성공")
+                .build();
+        return new ResponseEntity<>(body, Response.getDefaultHeader(), HttpStatus.OK);              // 존재하는 id 이고, 패스워드도 올바르다면 -> createToken 함수로 유저 id 정보를 담은 JWT 토큰 반환
+                                                                                                    // 이후 클라이언트는 이 JWT 토큰을 'X-AUTH-TOKEN' 필드에 넣어서 요청
+
     }
 }
